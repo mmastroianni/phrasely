4dfd1919be62fa73667e6a39859df20818181090
mastroianni <mmastroianni@gmail.com>
2025-10-18 18:30:31 -0400

    fixed failing unit tests

--- tests/reduction/test_visualization_reducer.py
+++ tests/reduction/test_visualization_reducer.py
@@ -6,7 +6,7 @@ from phrasely.reduction.visualization_reducer import UMAPReducer
 def test_umap_reducer_shape_cpu():
     """UMAP (CPU) should reduce correctly to 2D."""
     X = np.random.rand(300, 50).astype(np.float32)
-    reducer = UMAPReducer(method="umap", n_components=2, use_gpu=False)
+    reducer = UMAPReducer(n_components=2, use_gpu=False)
     Y = reducer.reduce(X)
     assert Y.shape == (300, 2)
     assert not np.isnan(Y).any()
